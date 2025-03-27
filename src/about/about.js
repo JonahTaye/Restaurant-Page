e5440dbc77b36cdd75bf48dfad1f1e3b2d0f9ce5
jonah <jonah.softwaredev@gmail.com>
2025-03-27 08:12:10 +0300

    feat: add about module and style for the about page

---
+++ src/about/about.js
@@ -0,0 +1,24 @@
+export const loadAboutPage = function () {
+    const mainContentDiv = document.getElementById("content")
+    
+    const contentContainer = document.createElement("div")
+    contentContainer.classList.add("about-container")
+    mainContentDiv.append(contentContainer)
+
+    const header = document.createElement("h1")
+    header.classList.add("slogan-header")
+    header.textContent = "BEST ORGANIC COFFEE"
+    contentContainer.appendChild(header)
+
+    const para = document.createElement("p")
+    para.className.add("about-para")
+    para.textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Quia culpa facilis ea laborum voluptatum? Laborum, culpa et. Nesciunt, fugit pariatur! 
+                        A distinctio quam dicta, explicabo voluptate non? Consequuntur, animi voluptatibus!`
+    contentContainer.appendChild(para)
+
+    const button = document.createElement("button")
+    button.className.add("about-btn")
+    button.textContent = "Visit Us"
+    contentContainer.appendChild(button)
+}
